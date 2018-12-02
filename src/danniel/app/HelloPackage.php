a09649d2371380aa1cdbddb6edd559bc6d9f834f
Danniel Covo <>
2018-12-02 16:56:25 -0200

    Files my composer

---
+++ src/danniel/app/HelloPackage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dannielcovo
+ * Date: 01/12/18
+ * Time: 20:32
+ */
+
+namespace danniel\app;
+class HelloPackage {
+	public function getHello(){
+		return "My egg Package!";
+	}
+}
